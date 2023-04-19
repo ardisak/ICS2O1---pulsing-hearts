4c4486d359572f1b7c270383c8eaef4bd61039ce
ardisak <131181965+ardisak@users.noreply.github.com>
2023-04-19 11:41:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
